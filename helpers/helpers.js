4e6f64fb45347e61ec81c5bab0a696b5a9c770ca
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-03-08 17:25:56 +0000

    Listings schema changed for datesvar today = new Date();

---
+++ helpers/helpers.js
@@ -0,0 +1,19 @@
+/**Coverts date to SQL format
+ *
+ * @returns SQL formated date
+ */
+const todayDate = () => {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  today = yyyy + '-' + mm + '-' + dd;
+  return today
+}
