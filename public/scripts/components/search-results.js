78edf685c99877e72e7f2788f2474b087f45d1fe
djtwdix <djtwd.ix@icloud.com>
2021-03-08 16:42:40 -0400

    Remove 'x' from search field in navbar

--- public/scripts/components/search-results.js
+++ public/scripts/components/search-results.js
@@ -34,37 +34,6 @@ const searchResults = `<div class="listings-page">
        </div>
      </div>
    </div>
-   <div class="listings-item">
-     <div class=listings-image>
-       <img src="/public/images/start.jpeg">
-     </div>
-     <div class="listings-info">
-       <div class="listings-mid">
-         <div class="specs">
-           <div class="listings-make">
-             Fender
-           </div>
-           <div class="listings-model">
-            Stratocastor
-           </div>
-          </div>
-          <div class="listings-price">
-            $829.99
-          </div>
-       </div>
-        <div class="listings-description">
-            Selling used fender Stratocastor....
-        </div>
-      </div>
-      <div class="listings-footer">
-        <div>
-         <i class="fas fa-heart"></i>
-        </div>
-        <div>
-          <i class="fas fa-envelope"></i>
-        </div>
-      </div>
-    </div>
 </div>
 </div>`
 
