e404f4670096fb5dc9a317845ba44102275e7012
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-03-08 16:46:02 +0000

    DB schema added

---
+++ db/schema/listings.sql
@@ -0,0 +1,19 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS listings CASCADE;
+CREATE TABLE listings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  seller_id INTEGER REFERENCES users(id),
+  description TEXT,
+  cost DECIMAL(6,2) NOT NULL,
+  condition VARCHAR(255),
+  listing_date ****,
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  handed VARCHAR(255),
+  type VARCHAR(255),
+  listing_pic VARCHAR(255),
+  listing_thumbnail VARCHAR(255),
+  is_active BOOLEAN DEFAULT TRUE,
+  views NUMBER DEFAULT 0
+);
---
+++ db/schema/users.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  hashed_password VARCHAR(255),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255,)
+  city VARCHAR(255),
+  province VARCHAR(255),
+  profile_pic VARCHAR(255)
+);
