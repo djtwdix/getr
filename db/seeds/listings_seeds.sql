8306cbfac0c5e744afa0bdde760b2c7f1c1a1525
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-03-08 18:38:10 +0000

    user and listing temporary seeds added

---
+++ db/seeds/listings_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO listings(seller_id, cost, listing_date, brand) VALUES (1, 250.50, "2020-12-15", "Fender");
+INSERT INTO listings(seller_id, cost, listing_date, brand) VALUES (2, 250.50, "2021-01-23", "Squier");
+INSERT INTO listings(seller_id, cost, listing_date, brand) VALUES (3, 250.50, "2021-02-07", "Gibson");
+INSERT INTO listings(seller_id, cost, listing_date, brand) VALUES (2, 250.50, "2021-03-01", "Fender");
