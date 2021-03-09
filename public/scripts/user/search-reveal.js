bd8d545d65d4bdaa62468d7b188fe9285ed46fbb
djtwdix <djtwd.ix@icloud.com>
2021-03-09 08:23:45 -0400

    Set up landing page ejs

---
+++ public/scripts/user/search-reveal.js
@@ -0,0 +1,10 @@
+$(".search-icon").on("click", function() {
+  console.log("clicked")
+  if (search) {
+    $(".search-field").fadeOut("slow")
+    search = false;
+  } else {
+    $(".search-field").fadeIn("slow")
+    search = true;
+  }
+})
