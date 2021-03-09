1ad8e85a67aafcf59569e14d9e00b7419fe7225e
djtwdix <djtwd.ix@icloud.com>
2021-03-09 13:45:13 -0400

    Get data from registration form into user obj

---
+++ public/scripts/user-registration.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  console.log("registration")
+  $("#register-form").on("submit", function(event) {
+    console.log("submitted")
+    event.preventDefault();
+    const first_name = $("#reg-first-name").val();
+    const last_name = $("#reg-last-name").val();
+    const email = $("#reg-email").val();
+    const password = $("#reg-password").val();
+    const profile_pic = $("#reg-photo").val();
+    const userInfo = {
+      first_name,
+      last_name,
+      email,
+      password,
+      profile_pic
+    }
+    console.log(userInfo);
+  })
+})
--- server.js
+++ server.js
@@ -71,9 +71,13 @@ app.get("/login", (req, res) => {
 })
 
 app.get("/register", (req, res) => {
+
   res.render("register")
 })
 
+app.post("/register", (req, res) => {
+  AudioScheduledSourceNode()
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
