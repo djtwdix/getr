827bf474a8d7b65b1ee60c14951a33616cb22fc2
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-03-09 18:12:24 +0000

    users seeds modified

--- db/seeds/users_seeds.sql
+++ db/seeds/users_seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO users VALUES(1, 'guy1@gmail.com', 'password', 'guy', '1', 'Toronto', 'Ontario', 'Canada', 'https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg');
-INSERT INTO users VALUES(2, 'guy2@gmail.com', 'password', 'guy', '1', 'Montreal', 'Ontario', 'Canada', 'https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg');
-INSERT INTO users VALUES(3, 'guy3@gmail.com', 'password', 'guy', '1', 'Edmonton', 'Alberta', 'Canada', 'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg');
+INSERT INTO users VALUES(2, 'guy2@gmail.com', 'password', 'guy', '2', 'Montreal', 'Ontario', 'Canada', 'https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg');
+INSERT INTO users VALUES(3, 'guy3@gmail.com', 'password', 'guy', '3', 'Edmonton', 'Alberta', 'Canada', 'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg');
 
