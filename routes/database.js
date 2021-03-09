0f9b9fc62447677991d426f7aeecb662c15df399
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-03-09 17:16:29 +0000

    addUser function added

---
+++ routes/database.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+const dbParams = require('../lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+const addUser = (user) => {
+  return db.query(`
+  INSERT INTO users (email, hashed_password, first_name, last_name, city, province, country, profile_pic)
+  VALUE ($1, $2, $3, $4, $5, $6, $7, $8)
+  RETURNING *;
+  `, [user.email, user.password, user.first_name, user.last_name, user.city, user.province, user.country, user.profile_pic])
+  .then(res => {
+    return res.rows[0];
+  })
+  .catch(err => err);
+}
+exports.addUser = addUser;
