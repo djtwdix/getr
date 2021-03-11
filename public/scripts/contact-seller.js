fc38509abbfece6ec2750ac25635d827370ee6ca
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-03-11 18:59:04 +0000

    remove older function

--- public/scripts/contact-seller.js
+++ public/scripts/contact-seller.js
@@ -1,15 +1,3 @@
-/* function contact() {
-  const form = document.getElementById("contact-seller-form");
-  if (form.style.display === "none") {
-    form.style.display = "block";
-    form.scrollIntoView();
-
-  } else {
-    form.style.display = "none";
-  }
-}
- */
-
 $(document).ready(function(){
   $("#contact-seller").on("click", function(){
     $("#contact-seller-form").slideToggle();
