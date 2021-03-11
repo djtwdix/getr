9290774ef93d48dd047dcb7a853516a324e78daa
djtwdix <djtwd.ix@icloud.com>
2021-03-11 08:41:43 -0400

    Toggle heart read on click and set up post req

--- public/scripts/favourite.js
+++ public/scripts/favourite.js
@@ -2,6 +2,21 @@ $(document).ready(function(){
   $(".favourite-icon").on("click", function(){
     console.log("clicked")
     $(this).toggleClass("red");
+    const listingId = $(this).find(".fave-item-id").text();
+    const userId = $(this).find(".fave-user-id").text();
+    const addToFaves = {
+      listingId,
+      userId
+    }
+
+    $.post("/favourite", addToFaves)
+      .done(data => {
+        console.log("Added to favourites");
+      })
+
+    console.log(addToFaves);
   })
 })
 
+/* "fave-item-id" style="display:none"><%= listingInfo[listing].id %> </div>
+                <div class="fave-user-id" */
