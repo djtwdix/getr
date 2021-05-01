cc1484e494368bb9a14da1580d439856918a2283
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-30 21:03:55 -0400

    clean up

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { sendOffer } = require("./helpers/email")
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
+const PORT       = process.env.PORT || 3000;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const bodyParser = require("body-parser");
